b1e516b48928be4bcd8696ea7aa5f9829eff92c7
Pravin Sankeshwari <pravin.sankeshwari@nykaa.com>
2025-02-23 21:12:45 +0530

    updated appsflyer version

--- Sources/NykaaDependencies/Dependencies.swift
+++ Sources/NykaaDependencies/Dependencies.swift
@@ -6,9 +6,5 @@ public struct Dependencies {
     public static let alamofireVersion = "5.6.4"
 
     // Analytics
-    public static let appsFlyerVersion = "6.11.0"
-
-    // Additional Libraries
-    public static let realmVersion = "10.20.2"
-    public static let firebaseVersion = "10.8.0"
+    public static let appsFlyerVersion = "6.15.0"
 }
